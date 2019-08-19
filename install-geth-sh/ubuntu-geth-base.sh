ad48882b162dc06f294f6be5be53f7293b020e54
pjt3591oo <pjt3591oo@gmail.com>
2019-08-19 16:29:49 +0900

    Adds geth build env scripts on Ubuntu

---
+++ install-geth-sh/ubuntu-geth-base.sh
@@ -0,0 +1,7 @@
+sudo apt-get install software-properties-common
+sudo add-apt-repository -y ppa:ethereum/ethereum
+sudo apt-get update
+sudo apt-get install ethereum
+
+sudo apt-get install -y build-essential
+
